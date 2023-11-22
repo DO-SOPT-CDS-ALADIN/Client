7610796e7450ec69c0ff222b4ec68a9e2eeb0600
urjimyu <92876819+urjimyu@users.noreply.github.com>
2023-11-23 03:58:38 +0900

    refactor: 이미지 추출 문법 수정

--- src/svg.d.ts
+++ src/svg.d.ts
@@ -11,9 +11,10 @@ declare module '*.png' {
 declare module '*.jpeg';
 
 declare module '*.svg?react' {
-  import React = require('react');
+  import * as React from 'react';
 
-  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
-  const src: string;
-  export default src;
+  export const ReactComponent: React.FunctionComponent<
+    React.ComponentProps<'svg'> & { title?: string }
+  >;
+  export default ReactComponent;
 }
