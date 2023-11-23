0cc0b1715d8546d04fa34da08c45abda5615d556
binllionaire <bug20474@gmail.com>
2023-11-24 02:55:53 +0900

    add : 태그 관련 상수 추가

---
+++ src/constants/tag.ts
@@ -0,0 +1,14 @@
+export const TAG = {
+  DELIVERY: {
+    ID: 1,
+    TEXT: '양탄자배송',
+  },
+  GIFT: {
+    ID: 2,
+    TEXT: '사은품증정',
+  },
+  RESERVATION: {
+    ID: 3,
+    TEXT: '예약판매',
+  },
+};
