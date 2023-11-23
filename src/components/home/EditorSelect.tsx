e76466a01ef12e5735a3442211440f01e72e2fa0
jungwoo3490 <jungwoo3490@naver.com>
2023-11-23 21:18:22 +0900

    feat: 편집장의 선택 책 정보 UI 구현

--- src/components/home/EditorSelect.tsx
+++ src/components/home/EditorSelect.tsx
@@ -1,18 +1,51 @@
 import styled from 'styled-components';
 import { useState, useRef, useEffect } from 'react';
 
-const imageSrcList = [
-  'https://image.aladin.co.kr/product/15133/99/cover200/8968331820_1.jpg?RS=170',
-  'https://image.aladin.co.kr/product/32800/29/cover200/8901276895_1.jpg?RS=170',
-  'https://image.aladin.co.kr/product/32386/47/cover200/k842935882_1.jpg?RS=170',
-  'https://image.aladin.co.kr/product/5267/77/cover200/8997221396_1.jpg?RS=170',
-  'https://image.aladin.co.kr/product/30457/49/cover200/k822830478_1.jpg?RS=170',
+const dummyData = [
+  {
+    title: `이 아이는 자라서 이렇게 됩니다.`,
+    subtitle: `"길에서 살아남아 성묘가 된 기적의 40냥이들"`,
+    content: `테스트1테스트1테스트1테스트1테스트1테스트1테스트1테스트1테스트1테스트1테스트1테스트1테스트1테스트1테스트1테스트1테스트1테스트1테스트1테스트1`,
+    imageSrc: `https://image.aladin.co.kr/product/32386/47/cover200/k842935882_1.jpg?RS=170`,
+  },
+  {
+    title: `테스트2`,
+    subtitle: `"테스트2"`,
+    content: `테스트2테스트2테스트2테스트2테스트2테스트2테스트2테스트2테스트2테스트2테스트2테스트2테스트2테스트2테스트2테스트2테스트2`,
+    imageSrc: `https://image.aladin.co.kr/product/32386/47/cover200/k842935882_1.jpg?RS=170`,
+  },
+  {
+    title: `테스트3`,
+    subtitle: `테스트3`,
+    content: `테스트3테스트3테스트3테스트3테스트3테스트3테스트3테스트3테스트3테스트3테스트3테스트3테스트3테스트3테스트3테스트3테스트3`,
+    imageSrc: `https://image.aladin.co.kr/product/32386/47/cover200/k842935882_1.jpg?RS=170`,
+  },
+  {
+    title: `테스트4`,
+    subtitle: `테스트4`,
+    content: `테스트4테스트4테스트4테스트4테스트4테스트4테스트4테스트4테스트4테스트4테스트4테스트4테스트4테스트4테스트4`,
+    imageSrc: `https://image.aladin.co.kr/product/32386/47/cover200/k842935882_1.jpg?RS=170`,
+  },
+  {
+    title: `테스트5`,
+    subtitle: `테스트5`,
+    content: `테스트5테스트5테스트5테스트5테스트5테스트5테스트5테스트5테스트5테스트5테스트5테스트5테스트5테스트5테스트5테스트5테스트5테스트5`,
+    imageSrc: `https://image.aladin.co.kr/product/32386/47/cover200/k842935882_1.jpg?RS=170`,
+  },
 ];
 
+// const imageSrcList = [
+//   'https://image.aladin.co.kr/product/15133/99/cover200/8968331820_1.jpg?RS=170',
+//   'https://image.aladin.co.kr/product/32800/29/cover200/8901276895_1.jpg?RS=170',
+//   'https://image.aladin.co.kr/product/32386/47/cover200/k842935882_1.jpg?RS=170',
+//   'https://image.aladin.co.kr/product/5267/77/cover200/8997221396_1.jpg?RS=170',
+//   'https://image.aladin.co.kr/product/30457/49/cover200/k822830478_1.jpg?RS=170',
+// ];
+
 // 슬라이드 마지막 인덱스
-const TOTAL_SLIDES = imageSrcList.length - 1;
+const TOTAL_SLIDES = dummyData.length - 1;
 // 이미지 컨테이너 너비
-const containerWidth = `${imageSrcList.length * 11.2}rem`;
+const containerWidth = `${dummyData.length * 11.2}rem`;
 
 // 왼쪽 버튼 클릭 이벤트 핸들러
 
@@ -40,14 +73,14 @@ function EditorSelect() {
 
   useEffect(() => {
     slideRef.current.style.transition = 'all 0.5s ease-in-out';
-    slideRef.current.style.transform = `translateX(-${currentSlide * 15}rem)`;
+    slideRef.current.style.transform = `translateX(-${currentSlide * 15.8}rem)`;
   }, [currentSlide]);
 
   return (
     <EditorSelectWrapper>
       <EditorSelectBookImgContainer ref={slideRef}>
-        {imageSrcList.map((src, index) => (
-          <EditorSelectBookImg key={index} src={src} isCurrent={index === currentSlide} />
+        {dummyData.map(({ imageSrc }, index) => (
+          <EditorSelectBookImg key={index} src={imageSrc} isCurrent={index === currentSlide} />
         ))}
       </EditorSelectBookImgContainer>
       <PrevButton onClick={PrevSlide} className="prev_button">
@@ -56,7 +89,11 @@ function EditorSelect() {
       <NextButton onClick={NextSlide} className="next_button">
         ＞
       </NextButton>
-      <BookInfoContainer></BookInfoContainer>
+      <BookInfoContainer>
+        <Title>{dummyData[currentSlide].title}</Title>
+        <SubTitle>{dummyData[currentSlide].subtitle}</SubTitle>
+        <DetailContainer>{dummyData[currentSlide].content}</DetailContainer>
+      </BookInfoContainer>
     </EditorSelectWrapper>
   );
 }
@@ -75,13 +112,13 @@ const EditorSelectBookImgContainer = styled.section`
   width: ${containerWidth}px;
   display: flex;
   column-gap: 4rem;
-  padding-left: 11rem;
+  padding-left: 11.5rem;
   margin-top: 2rem;
 `;
 
 const EditorSelectBookImg = styled.img<{ isCurrent: boolean }>`
-  width: ${props => (props.isCurrent ? '16rem' : '11.2rem')};
-  height: ${props => (props.isCurrent ? '20.8rem' : '16rem')};
+  width: ${props => (props.isCurrent ? '14.7rem' : '11.8rem')};
+  height: ${props => (props.isCurrent ? '21.5rem' : '17.2rem')};
   margin-top: ${props => (props.isCurrent ? '0' : '4.8rem')};
   object-fit: cover;
   position: relative;
@@ -105,4 +142,27 @@ const BookInfoContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+`;
+
+const Title = styled.p`
+  ${({ theme }) => theme.fonts.title1_bold};
+  color: white;
+  margin-top: 2.4rem;
+`;
+
+const SubTitle = styled.p`
+  ${({ theme }) => theme.fonts.detail2};
+  color: white;
+  margin-top: 0.2rem;
+`;
+
+const DetailContainer = styled.div`
+  ${({ theme }) => theme.fonts.body2};
+  width: 91%;
+  margin-top: 1.5rem;
+  height: 10.8rem;
+  padding: 1.4rem 1.6rem;
+  color: white;
+  background-color: ${({ theme }) => theme.colors.translucent};
+  border-radius: ${({ theme }) => theme.radius.s};
 `;
