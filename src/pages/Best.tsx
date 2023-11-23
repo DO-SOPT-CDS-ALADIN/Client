25db17aef19aa267c4c60f9784d92faf84fc60a9
binllionaire <bug20474@gmail.com>
2023-11-24 03:01:41 +0900

    feat : 베스트 페이지 UI 구현

--- src/pages/Best.tsx
+++ src/pages/Best.tsx
@@ -1,20 +1,39 @@
+import styled from 'styled-components';
+import Book from '../components/best/Book';
 import Category from '../components/best/Category';
+import Pagenation from '../components/best/Pagenation';
 import Period from '../components/best/Period';
+import BackButton from '../components/common/BackButton';
+import BottomNav from '../components/common/BottomNav';
 import CategoryNav from '../components/common/CategoryNav';
+import Footer from '../components/common/Footer';
 import Header from '../components/common/Header';
+import TopButton from '../components/common/TopButton';
 import TopNav from '../components/common/TopNav';
 import PAGE from '../constants/page';
 
 function Best() {
   return (
-    <>
+    <BestWrapper>
       <Header />
       <TopNav page={PAGE.BEST} />
       <Category />
       <CategoryNav />
       <Period />
-    </>
+      <Book tagCount={1} />
+      <Book tagCount={2} />
+      <Book tagCount={3} />
+      <Pagenation />
+      <Footer />
+      <BackButton />
+      <TopButton />
+      <BottomNav />
+    </BestWrapper>
   );
 }
 
 export default Best;
+
+const BestWrapper = styled.div`
+  padding: 10.8rem 0 6.8rem;
+`;
