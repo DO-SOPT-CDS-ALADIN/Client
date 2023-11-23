4624e09cae395310a841c91689cd7167d8297a63
binllionaire <bug20474@gmail.com>
2023-11-23 23:23:04 +0900

    add : 베스트 카테고리 배열 상수 추가

---
+++ src/constants/bestCategory.ts
@@ -0,0 +1,47 @@
+import {
+  IcBestDomestic,
+  IcBestOveraes,
+  IcBestOnline,
+  IcBestEBook,
+  IcBestGoods,
+  IcBestMusic,
+  IcBestMovie,
+} from '../assets/icons';
+
+export const BEST_CATEGORY = [
+  {
+    id: 1,
+    icon: IcBestDomestic,
+    category: '국내도서',
+  },
+  {
+    id: 2,
+    icon: IcBestOveraes,
+    category: '외국도서',
+  },
+  {
+    id: 3,
+    icon: IcBestOnline,
+    category: '온라인중고',
+  },
+  {
+    id: 4,
+    icon: IcBestEBook,
+    category: 'eBook',
+  },
+  {
+    id: 5,
+    icon: IcBestGoods,
+    category: '알라딘굿즈',
+  },
+  {
+    id: 6,
+    icon: IcBestMusic,
+    category: '음반',
+  },
+  {
+    id: 7,
+    icon: IcBestMovie,
+    category: '블루레이',
+  },
+];
