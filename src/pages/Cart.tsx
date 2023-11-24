ab6dfe00cb99884d6a96caf2ddab99975b926937
binllionaire <bug20474@gmail.com>
2023-11-25 03:02:58 +0900

    feat : 장바구니 상단 UI 구현

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,5 +1,19 @@
+import CartHeader from '../components/cart/CartHeader';
+import Filter from '../components/cart/Filter';
+import Nav from '../components/cart/Nav';
+import CartList from '../components/cart/CartList';
+import Receipt from '../components/cart/Receipt';
+
 function Cart() {
-  return <></>;
+  return (
+    <>
+      <CartHeader />
+      <Nav />
+      <Filter />
+      <CartList />
+      <Receipt />
+    </>
+  );
 }
 
 export default Cart;
