3a8058a40cd98dc999478a7b4de032153aa57fd4
binllionaire <bug20474@gmail.com>
2023-11-24 17:04:04 +0900

    feat : 활성화 된 페이지 표시 구현

--- src/components/best/Pagenation.tsx
+++ src/components/best/Pagenation.tsx
@@ -1,11 +1,16 @@
 import styled from 'styled-components';
 import { IcPagenationOver, IcPagenationNext } from '../../assets/icons';
 
+interface ButtonProps {
+  active: number;
+}
+
 function Pagenation() {
   const totalPages = 5;
+  const currentPage = 1;
   const pages = Array.from({ length: totalPages }, (_, index) => index + 1);
   return (
-    <PagenationWrapper>
+    <PagenationWrapper active={currentPage}>
       {pages.map(page => (
         <PagenationButtton key={page}>{page}</PagenationButtton>
       ))}
@@ -16,14 +21,6 @@ function Pagenation() {
 }
 export default Pagenation;
 
-const PagenationWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 0.2rem;
-
-  margin: 1.6rem auto;
-`;
 const PagenationButtton = styled.button`
   display: flex;
   width: 2.8rem;
@@ -33,6 +30,24 @@ const PagenationButtton = styled.button`
   justify-content: center;
   align-items: center;
   gap: 1rem;
-  border: 1px solid ${({ theme }) => theme.colors.grey_300};
-  font: ${({ theme }) => theme.fonts.body2};
+`;
+
+const PagenationWrapper = styled.div<ButtonProps>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.2rem;
+
+  margin: 1.6rem auto;
+
+  & ${PagenationButtton} {
+    border: 1px solid ${({ theme }) => theme.colors.grey_300};
+    font: ${({ theme }) => theme.fonts.body4};
+  }
+
+  & ${PagenationButtton}:nth-child(${props => props.active}) {
+    color: ${({ theme }) => theme.colors.blue_400};
+    border: 1px solid ${({ theme }) => theme.colors.blue_400};
+    font: ${({ theme }) => theme.fonts.body3};
+  }
 `;
