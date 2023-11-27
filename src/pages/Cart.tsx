023b9a8e3e9b69df3685172546bf46445270290f
binllionaire <bug20474@gmail.com>
2023-11-28 03:23:48 +0900

    feat : 장바구니 페이지 UI 구현

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -3,17 +3,35 @@ import Filter from '../components/cart/Filter';
 import Nav from '../components/cart/Nav';
 import CartList from '../components/cart/CartList';
 import Receipt from '../components/cart/Receipt';
+import BuyWith from '../components/common/BuyWith';
+import AladinGoods from '../components/cart/AladinGoods';
+import styled from 'styled-components';
+import AladinCoffee from '../components/cart/AladinCoffee';
+import Footer from '../components/common/Footer';
+import BackButton from '../components/common/BackButton';
+import BottomBar from '../components/cart/BottomBar';
 
 function Cart() {
   return (
-    <>
+    <CartWrapper>
       <CartHeader />
       <Nav />
       <Filter />
       <CartList />
       <Receipt />
-    </>
+      <BuyWith />
+      <AladinGoods />
+      <AladinCoffee />
+      <Footer />
+      <BackButton />
+      <BottomBar />
+    </CartWrapper>
   );
 }
 
 export default Cart;
+
+const CartWrapper = styled.div`
+  background-color: ${({ theme }) => theme.colors.grey_200};
+  padding-bottom: 10.8rem;
+`;
