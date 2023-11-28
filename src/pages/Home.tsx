9423551d2b39282b37779fab435d053432cf65f0
jungwoo3490 <jungwoo3490@naver.com>
2023-11-28 11:49:05 +0900

    refactor: 배너 컴포넌트 src constants 파일로 분리

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ import EditorSelect from '../components/home/EditorSelect';
 import TopNav from '../components/common/TopNav';
 import SlideBanner from '../components/home/SlideBanner';
 import TopTen from '../components/home/TopTen';
-import { FIRST_BANNER_IMAGE_LINKS } from '../constants/home';
+import { FIRST_BANNER_IMAGE_LINKS, MIDDLE_BANNER_IMAGE_LINK } from '../constants/home';
 import HotBook from '../components/home/HotBook';
 import MadeGift from '../components/home/MadeGift';
 
@@ -19,7 +19,7 @@ function Home() {
       <EditorSelect />
       <TopTen />
       <BannerImgWrapper>
-        <BannerImg src="https://s3-alpha-sig.figma.com/img/ef11/d058/412be2d01c68875c3285be20db729bc3?Expires=1702252800&Signature=X0TVH~fK1MVn~KFt~BrIa0QawjeCyZU55jaS9vKHjYTmDIsscCcX1EVwNDrif6NdiH~MJ4b6j8UKyLx8dTJc5k0c1ooeIqM1DFC2Rns1x-Y6HHaR3Y70pCuL6Zu3xS9qMrlwjbOYSAvsTXTGxY6m3rJNq-4gwluiggLO0A4Q2dl~2v-n8w88dkNyJfPceJywt~WZnhkgQY35NBR3JLaRNQ~ltd830kn3vSYzTJMtJ7jxuTeqTCQt~tNj4lH2Vkg7AfwGwax-rE733cyN1jirZjyaE63P6BTAQP7jI9eNGwxq~RYP4rMAhKdOvYzz5JPN2aV-QIwJv0kDDp7TNFn5pQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" />
+        <BannerImg src={MIDDLE_BANNER_IMAGE_LINK} />
       </BannerImgWrapper>
       <HotBook />
       <MadeGift />
