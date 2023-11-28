b637abe11158ac6ad2227dec4c379540ff86f892
urjimyu <92876819+urjimyu@users.noreply.github.com>
2023-11-29 02:11:09 +0900

    chore: 바뀐 파일 경로 반영

--- src/components/detail/DetailPostReview.tsx
+++ src/components/detail/DetailPostReview.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import { IcCheckboxGray, IcEnterXsGrey, IcStarLargePink } from '../../assets/icons';
-import DETAIL_TEXTS from '../../constants/detail';
+import DETAIL_TEXTS from '../../constants/DETAIL_TEXTS';
 
 function DetailPostReview() {
   const [textAreaValue, setTextAreaValue] = useState('');
