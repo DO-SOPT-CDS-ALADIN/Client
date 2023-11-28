f8fb416ac64f4fb837ca088e2d18c4ac418f068f
binllionaire <bug20474@gmail.com>
2023-11-29 03:48:29 +0900

    add : BookProps 추가

---
+++ src/utils/BookProps.ts
@@ -0,0 +1,15 @@
+export interface BookProps {
+  rank: number;
+  imgUrl: string;
+  title: string;
+  subtitle: string;
+  writer: string;
+  painter: string;
+  publisher: string;
+  pubDate: string;
+  score: number;
+  tag: number;
+  discountPrice: string;
+  mileage: string;
+  heart: true;
+}
