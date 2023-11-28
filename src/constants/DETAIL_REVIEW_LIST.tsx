12985adb5341b4f89c20b0400dc1f5b2b776e6a9
urjimyu <92876819+urjimyu@users.noreply.github.com>
2023-11-28 15:41:30 +0900

    chore: 상수 파일 관리

---
+++ src/constants/DETAIL_REVIEW_LIST.tsx
@@ -0,0 +1,90 @@
+import DETAIL_TEXTS from './DETAIL_TEXTS';
+
+const DETAIL_REVIEW_LIST = [
+  {
+    userId: 'appletreeje',
+    date: '2023-10-31',
+    content: `${DETAIL_TEXTS.REVIEW_CONTENTS_1}`,
+    agreeNum: 33,
+    commentNum: 0,
+  },
+  {
+    userId: '철철대마왕',
+    date: '2023-10-29',
+    content: `${DETAIL_TEXTS.REVIEW_CONTENTS_2}`,
+    agreeNum: 3,
+    commentNum: 0,
+  },
+  {
+    userId: '진아님',
+    date: '2023-11-01',
+    content: `${DETAIL_TEXTS.REVIEW_CONTENTS_3}`,
+    agreeNum: 1,
+    commentNum: 0,
+  },
+  {
+    userId: 'lemon_tear',
+    date: '2023-10-31',
+    content: `${DETAIL_TEXTS.REVIEW_CONTENTS_4}`,
+    agreeNum: 1,
+    commentNum: 0,
+  },
+  {
+    userId: 'galapagos55',
+    date: '2023-10-30',
+    content: `${DETAIL_TEXTS.REVIEW_CONTENTS_5}`,
+    agreeNum: 1,
+    commentNum: 0,
+  },
+  {
+    userId: 'lielka',
+    date: '2023-10-27',
+    content: `${DETAIL_TEXTS.REVIEW_CONTENTS_6}`,
+    agreeNum: 1,
+    commentNum: 0,
+  },
+  {
+    userId: 'hihihi',
+    date: '2023-10-10',
+    content: `${DETAIL_TEXTS.REVIEW_CONTENTS_1}`,
+    agreeNum: 21,
+    commentNum: 5,
+  },
+  {
+    userId: 'honeyweblover',
+    date: '2023-11-11',
+    content: `${DETAIL_TEXTS.REVIEW_CONTENTS_4}`,
+    agreeNum: 57,
+    commentNum: 25,
+  },
+  {
+    userId: 'cds1thebest',
+    date: '2023-10-20',
+    content: `${DETAIL_TEXTS.REVIEW_CONTENTS_2}`,
+    agreeNum: 30,
+    commentNum: 24,
+  },
+  {
+    userId: 'hungry',
+    date: '2023-11-29',
+    content: `${DETAIL_TEXTS.REVIEW_CONTENTS_5}`,
+    agreeNum: 2,
+    commentNum: 3,
+  },
+  {
+    userId: 'aladinnn',
+    date: '2023-10-07',
+    content: `${DETAIL_TEXTS.REVIEW_CONTENTS_4}`,
+    agreeNum: 0,
+    commentNum: 0,
+  },
+  {
+    userId: 'lalalalla',
+    date: '2023-11-28',
+    content: `${DETAIL_TEXTS.REVIEW_CONTENTS_3}`,
+    agreeNum: 4,
+    commentNum: 0,
+  },
+];
+
+export default DETAIL_REVIEW_LIST;
