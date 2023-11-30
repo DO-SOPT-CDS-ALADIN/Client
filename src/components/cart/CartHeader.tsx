9fd35b187a3e81529f2952d72c8fff70a61ba0d7
binllionaire <bug20474@gmail.com>
2023-12-01 06:12:51 +0900

    style : 장바구니 header 고정

--- src/components/cart/CartHeader.tsx
+++ src/components/cart/CartHeader.tsx
@@ -17,12 +17,19 @@ function CartHeader() {
 export default CartHeader;
 
 const HeaderWrapper = styled.div`
+  position: fixed;
+  z-index: 10;
+  top: 0;
+  right: 0;
+  left: 0;
+
   display: flex;
   align-items: center;
   justify-content: space-between;
 
   width: 37.5rem;
   height: 4.8rem;
+  margin: 0 auto;
   padding-left: 1.2rem;
 
   background-color: ${({ theme }) => theme.colors.white};
