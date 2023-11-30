840d54f5f3f8cd9a821dab19655aca82de88d16b
binllionaire <bug20474@gmail.com>
2023-12-01 00:43:31 +0900

    feat : 전체 선택/ 개별 선택 구현

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,5 +1,4 @@
 import CartHeader from '../components/cart/CartHeader';
-import Filter from '../components/cart/Filter';
 import Nav from '../components/cart/Nav';
 import CartList from '../components/cart/CartList';
 import Receipt from '../components/cart/Receipt';
@@ -17,7 +16,6 @@ function Cart() {
     <CartWrapper>
       <CartHeader />
       <Nav />
-      <Filter />
       <CartList />
       <Receipt />
       <BuyWith />
