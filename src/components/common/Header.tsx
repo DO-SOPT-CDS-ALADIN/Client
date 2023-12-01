39b2d22d6053a1e2514f307ae7264eea7698a090
jungwoo3490 <jungwoo3490@naver.com>
2023-12-01 16:14:44 +0900

    feat: Header에서 로고 클릭했을 때 Home 페이지로 이동 기능 구현

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -11,7 +11,7 @@ function Header() {
 
   return (
     <HeaderWrapper>
-      <LogoButton>
+      <LogoButton onClick={() => navigate('/')}>
         <IcLogo />
       </LogoButton>
       <SearchBar>
